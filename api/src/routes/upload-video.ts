dbf6751416d31162cd2c6ce987bae1422bc6155d
Kaue Alves <kaue_alves00@yahoo.com>
2023-09-18 18:05:18 -0300

    refactor/ don't save audio to database

--- api/src/routes/upload-video.ts
+++ api/src/routes/upload-video.ts
@@ -31,13 +31,13 @@ export async function uploadVideoRoute(app:FastifyInstance) {
 
     await pump(data.file, fs.createWriteStream(uploadDestination))
 
-    const video = await prisma.video.create({
-      data: {
-        name: data.filename,
-        path: uploadDestination,
-      }
-    })
+    // const video = await prisma.video.create({
+    //   data: {
+    //     name: data.filename,
+    //     path: uploadDestination,
+    //   }
+    // })
     
-    return { video }
+    // return { video }
   })
 }
